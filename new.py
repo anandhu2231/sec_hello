292dc9e7b2b44b6fb0ff7a4ed282b4c87c6c4eaf
Anandhu2231 <40719852+anandhu2231@users.noreply.github.com>
2023-01-10 23:34:54 +0530

    add2

--- new.py
+++ new.py
@@ -1,11 +1,24 @@
 def sum():
-        a = (input("Enter number1 : "))
-        b = (input("enter number2 : "))
-        try:
+        operation = input("select - int, float, others - ")
+        if operation == "int":
+            a = int(input("Enter number1 : "))
+            b = int(input("enter number2 : "))
             sum = a+b
             return sum
-        except ValueError:
-            return("Input Value error")
+        elif operation == "float":
+            a = float(input("Enter number1 : "))
+            b = float(input("enter number2 : "))    
+            sum = a+b
+            return sum
+        elif operation == "other":
+            a = str(input("Enter number1 : "))
+            b = str(input("enter number2 : "))
+            return a+b
+        else:
+            return "Wrong input"    
+
+
+
 
 
 
